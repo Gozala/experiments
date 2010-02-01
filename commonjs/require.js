3df0cc6ec90b5c68f38ced2ca5d3b92640cbd9e7
Irakli Gozalishvili <rfobic@gmail.com>
2010-02-01 22:29:55 +0100

    Updating commonjs module loader so now it has a setup for new contexts

--- commonjs/require.js
+++ commonjs/require.js
@@ -2,15 +2,22 @@
  * An implementation of the CommonJS Modules 1.0
  * Copyright (c) 2009 by Irakli Gozalishvili
  */
-(function(exports, global, module) {
+var require = function require(id) { // wiil be replaced by require.setup
+    return require.setup({ global: window })(id);
+};
+require.main = function main(id) { // wiil be replaced by require.setup
+    return require.setup({ global: window }).main(id);
+}
+require.setup = function setup(properties) {
     var REQUIRE_MATCH = /require\s*\(('|")([\w\W]*?)('|")\)/mg;
-    var COMMENTS_MATCH = /(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g;
-    var modules = {};
-    var factories = {};
-    var sources = {};
-    var waiting = [];
-    var metadata = {};
-    var main;
+    var COMMENTS_MATCH = /(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|((^|\n)[^\'\"\n]*\/\/[^\n]*)/g;
+    var global = properties.global || {};
+    var prefix = properties.prefix || "";
+    var modules = properties.modules || {};
+    var factories = properties.factories || {};
+    var sources = properties.sources || {};
+    var system = modules[prefix + "system"] = properties.system || { print: function print() {} };
+    var waiting = [], metadata = {}, main;
 
     function Promise() {}
     Promise.prototype.when = function() {};
@@ -20,14 +27,16 @@
         load(id);
         return modules[id];
     }
-    function require(id) {
+    var requirer = function require(id) {
         return sandbox(id);
     }
-    require.main = function(id) {
-        main = metadata[id] = metadata[id] || {};
-        sandbox(id);
+    requirer.main = function(id) {
+        id = loader.resolve(id, prefix);
+        requirer.main = main = metadata[id] = metadata[id] || {};
+        load(id);
         return modules[id];
     };
+    requirer.setup = setup;
     function load(id) {
         var promise = new Promise();
         var path = id + ".js";
@@ -35,14 +44,19 @@
             if (!factories[id]) {
                 var require = Require(id);
                 var exports = modules[id];
-                var module = metadata[id] = metadata[id] || {};
+                var module = metadata[id] || (metadata[id] = {});
                 module.id = id;
                 module.path = path;
-                module.toString = function () {
-                    return this.id;
-                };
-                var factory = factories[id] = eval(sources[id]);
-                factory.call({}, require, exports, module);
+                module.toString = function () { return this.id; };
+                try {
+                    var factory = factories[id] = eval(sources[id]);
+                    factory.call({}, require, exports, module, system, system.print);
+                } catch(e) {
+                    var lineNumber = lineNumber || e.lineno || e.line || e.lineNumber;
+                    e.lineno = e.line = e.lineNumber = lineNumber;
+                    e.filename = e.fileName = e.sourceURL = module.path;
+                    throw e;
+                }
             }
             setTimeout(function() {
                 promise.when();
@@ -50,21 +64,23 @@
         } else {
             modules[id] = {};
             waiting.push(id);
-            loader.fetch(path).when = function(source) {
-                var dependency, dependencies = depends(sources[id] = source);
+            loader.fetch(path).when = function(source, text) {
+                var dependency, dependencies = depends(text);
+                sources[id] = source;
                 while (dependency = dependencies.shift()) {
                     dependency = loader.resolve(dependency, id);
                     if (waiting.indexOf(dependency) >= 0 || sources[dependency]) continue;
                     waiting.push(dependency);
                     load(dependency).when = (function(dependency) {
                         waiting.splice(waiting.indexOf(dependency), 1);
-                        if (0 == waiting.length) {
+                        if (!waiting.length) {
                             load(id);
                             promise.when();
                         }
                     });
                 }
                 waiting.splice(waiting.indexOf(id), 1);
+                if (!waiting.length) load(id);
                 promise.when();
             };
         }
@@ -79,14 +95,16 @@
 
     var loader = {
         resolve: function(id, baseId) {
-            if (!baseId || 0 < id.indexOf("://")) return id;
+            if (0 < id.indexOf("://")) return id;
+            if (id.charAt(0) != ".") return prefix + id;
+            baseId = baseId || prefix;
             var base = baseId.split("/");
             base.pop();
             var part, parts = id.split("/");
             while (part = parts.shift()) {
                 if (part == ".") continue;
-                if (part == "..") base.pop();
-                else base.push(part)
+                if (part == ".." && base.length) base.pop();
+                else base.push(part);
             }
             return base.join("/");
         },
@@ -97,9 +115,10 @@
             xhr.onreadystatechange = function() {
                 if (xhr.readyState == 4) {
                     if((xhr.status == 200 || xhr.status == 0) && xhr.responseText != "") {
+                        var text = xhr.responseText;
                         var source = "(function(require, exports, module, system, print) { "
-                                + xhr.responseText + "})\n//@ sourceURL=" + path;
-                        promise.when(source);
+                                + text + "})\n//@ sourceURL=" + path;
+                        promise.when(source, text);
                     } else {
                         throw new Error("Cant fetch module from: " + path);
                     }
@@ -109,7 +128,6 @@
             return promise;
         }
     };
-
     function Require(baseId) {
         var require = function(id) {
             return sandbox(id, baseId);
@@ -117,7 +135,7 @@
         require.main = main;
         return require;
     }
-
-    exports.require = require;
-})(this, this)
-
+    // overriding global require in case if it' pre setuped one
+    if (requirer.toString() != require.toString()) require = requirer;
+    return requirer;
+};
